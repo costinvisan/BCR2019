95bc115782298ac064bd0898b5f519c96ae70753
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2019-03-26 10:44:43 +0200

    asljdlksajdas

---
+++ BCR2019@TBP/src/main/java/com/BCR2019/BCR2019/TBP/Application.java
@@ -0,0 +1,13 @@
+package com.BCR2019.BCR2019.TBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
